c42715c01f63ad426f7fff9a866b121473cb6c3a
LosBunkos <nadavvadan@gmail.com>
2016-08-16 13:26:21 +0300

    fuck projectiles!

--- gameobj.js
+++ gameobj.js
@@ -13,25 +13,25 @@ var gameObj = function(board, startingPos) {
   }
 };
 
-var Projectile = function(board, startingPos, direction, speed) {
-  this.type = 3;
-  this.speed = speed;
-  this.pos = startingPos;
-  this.direction = direction;
-  this.go = function(direction){
-    board.go(this, direction);
-    return true;
-  };
-  this.onCollision = function(obj) {
-    board.delObj(this);
-  }
+// var Projectile = function(board, startingPos, direction, speed) {
+//   this.type = 3;
+//   this.speed = speed;
+//   this.pos = startingPos;
+//   this.direction = direction;
+//   this.go = function(direction){
+//     board.go(this, direction);
+//     return true;
+//   };
+//   this.onCollision = function(obj) {
+//     board.delObj(this);
+//   }
 
-  var that = this;
-  this.autoAct = function() {
-    setInterval(function() {
-      console.log(that);
-      that.go(direction);
-    }, speed);
-  // this.autoAct();
-}
-}
+//   var that = this;
+//   this.autoAct = function() {
+//     setInterval(function() {
+//       console.log(that);
+//       that.go(direction);
+//     }, speed);
+//   // this.autoAct();
+// }
+// }
